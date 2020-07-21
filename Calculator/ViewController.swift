db6803be075fd3a061d1893c3e8daf88264fa400
WonGi-Kim <kwg970310@gmail.com>
2020-07-21 15:01:53 +0900

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by kimwongi on 2020/07/21.
+//  Copyright © 2020 kimwongi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
